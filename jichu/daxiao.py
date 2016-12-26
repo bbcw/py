a892a001e40e83f593b7af09bd9a75309e8ca3aa
wen <373636124@qq.com>
2016-12-26 18:04:13 +0800

    1234

---
+++ jichu/daxiao.py
@@ -0,0 +1,37 @@
+#coding:utf-8
+
+import random
+def roll_dice(numbers=3,points=None):
+    print('<<<<< ROLL THE DICE! >>>>>')
+    if points is None:
+        points = []
+    while numbers >0:
+        point = random.randrange(1,7)
+        points.append(point)
+        numbers = numbers - 1
+    return points
+
+def roll_result(total):
+    isBis = 11 <= total <= 18
+    isSmall = 3<= total <= 10
+    if isBis:
+        return 'Big'
+    elif isSmall:
+        return 'Small'
+
+def start_game():
+    print('<<<<< GAME STARTS! >>>>>')
+    choices = ['Big','Small']
+    your_choice = input('Big or Small: ')
+    if your_choice in choices:
+        points = roll_dice()
+        total = sum(points)
+        youWin = your_choice == roll_result(total)
+        if youWin:
+            print('The points are',points,'You win !')
+        else:
+            print('The points are',points,'You lose !')
+    else:
+        print('Invalid words')
+        start_game()
+start_game()
