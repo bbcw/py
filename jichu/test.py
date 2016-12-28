7c80ceb4c3bb586f87a6429a4dabc1a0793e8ff2
wen <373636124@qq.com>
2016-12-28 21:56:50 +0800

    12-28

--- jichu/test.py
+++
@@ -1,5 +0,0 @@
-#coding:utf-8
-
-lyric = 'The night begin to shine, the night begin to shine'
-words = lyric.split()
-print(words)
---
+++ test.py
@@ -0,0 +1,5 @@
+#coding:utf-8
+
+lyric = 'The night begin to shine, the night begin to shine'
+words = lyric.split()
+print(words)
